9e07c1c94925ffb29c1169fac28dd97365fe4e6f
skalo06 <taha.000.kech@gmail.com>
2023-05-05 19:02:57 +0100

    adding field role in user table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
                 if (Auth()->attempt($credentials)) {
                 $user = Auth::user();
                 $token = $user->createToken('app')->accessToken;
-                return response()->json(['_id' => $user->id,"isAdmin"=>$user->isAdmin,"username"=>$user->name,"token"=>$token]);
+                return response()->json(['_id' => $user->id,"role"=>$user->role,"username"=>$user->name,"token"=>$token]);
                 } 
                 else {
                   return response()->json(['error' => 'Unauthenticated'], 401);
