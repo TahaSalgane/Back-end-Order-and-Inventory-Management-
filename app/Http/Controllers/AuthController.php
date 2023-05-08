4fd0464633ec8441b5dc268c57ee62adacf1fbfc
skalo06 <taha.000.kech@gmail.com>
2023-05-08 04:27:41 +0100

    add field image user login response image user controller update the profile ( image,  password , username)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,30 +8,40 @@ use Exception;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
 // use Laravel\Passport\Passport;
 // use Laravel\Sanctum\HasApiTokens;
 class AuthController extends Controller
 {
     //
     public function login(Request $request)
-        {
-             try{
-                $credentials = ['email' => $request->email,'password' => $request->password];
-
-                if (Auth()->attempt($credentials)) {
+    {
+        try {
+            $credentials = ['email' => $request->email, 'password' => $request->password];
+    
+            if (Auth()->attempt($credentials)) {
                 $user = Auth::user();
                 $token = $user->createToken('app')->accessToken;
-                return response()->json(['_id' => $user->id,"role"=>$user->role,"username"=>$user->name,"token"=>$token]);
-                }
-                else {
-                  return response()->json(['error' => 'Incorrect Email or Password !'], 401);
-                 }
-              }
-             catch(Exception $ex){
-               return response([
-                 'message'=> $ex->getMessage()
-                ],400);}
+                $profilePicture = $user->image ? Storage::url($user->image) : null;
+    
+                return response()->json([
+                    '_id' => $user->id,
+                    'role' => $user->role,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'profile_image' => $profilePicture
+                ]);
+            } else {
+                return response()->json(['error' => 'Incorrect Email or Password!'], 401);
+            }
+        } catch (Exception $ex) {
+            return response([
+                'message' => $ex->getMessage()
+            ], 400);
+        }
     }
+    
 
     public function Register(Request $request){
         try{
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
     public function user(){
         return Auth::user();
     }
-    public function updateProfile(){
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+    
+        // Update name, email, and password
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+    
+        // Handle profile image
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+    
+            // Delete previous image if exists
+            if ($user->image && Storage::exists($user->image)) {
+                Storage::delete($user->image);
+            }
+    
+            // Store new image
+            $imagePath = $image->store('profile-images', 'public');
+            $user->image = $imagePath;
+        }
+    
+        $user->save();
+    
         return response()->json([
-            'message' => 'hello from edit profile page !'
+            'message' => 'Profile updated successfully',
+            'user' => $user
         ]);
     }
+    
 }
